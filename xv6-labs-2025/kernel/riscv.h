ef6b56b177045dfe6299c7004586c3a0b898dba1
Tan Hiep Le <23120255@student.hcmus.edu.vn>
2025-12-04 09:59:36 +0700

    feat: define PTE_A

--- xv6-labs-2025/kernel/riscv.h
+++ xv6-labs-2025/kernel/riscv.h
@@ -374,8 +374,9 @@ typedef uint64 *pagetable_t; // 512 PTEs
 #define PTE_W (1L << 2)
 #define PTE_X (1L << 3)
 #define PTE_U (1L << 4) // user can access
-
-
+// Hiep 
+#define PTE_A (1L << 6) // accessed - hardware sets this
+#define PTE_D (1L << 7) // dirty
 
 #if defined(LAB_MMAP) || defined(LAB_PGTBL) || defined(LAB_COW)
 #define PTE_LEAF(pte) (((pte) & PTE_R) | ((pte) & PTE_W) | ((pte) & PTE_X))
